6753e7a1b75cc9a8f3d85a8c02e5fafe8d9ca323
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-28 12:48:24 -0700

    finished writing renderTodo(todo) func in render-utils.js file

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,20 @@
 export function renderTodo(todo) {
     // create a div and a p tag
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
+    div.classList.add(todo.complete ? 'complete' : 'incomplete');
 
     // add the 'todo' css class no matter what
+    div.classList.add('todo');
 
     // put the todo's text into the p tag
+    p.textContent = todo.todo;
 
     // append stuff
+    div.append(p);
 
     // return the div
+    return div;
 }
